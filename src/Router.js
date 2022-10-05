1a5778b8ef0ae2a07028c96ff29e76d4e85464f3
GyoungMin <101560583+Bae0203@users.noreply.github.com>
2022-10-05 10:48:52 +0900

    Routing
    기초 라우팅

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,8 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Main from "./components/Main";
 import Signin from "./components/signin";
 import Signup from "./components/signup";
+import Create from "./components/Create";
+import Search from "./components/Search";
 
 const Router = () => {
   return (
@@ -11,6 +13,8 @@ const Router = () => {
         <Route path="/" element={<Main />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/CreateProj" element={<Create />} />
+        <Route path="/SearchProj" element={<Search />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,6 +7,8 @@ const Main = () => {
       <div>Main</div>
       <CusBtn href="/Signin" location="로그인" />
       <CusBtn href="/Signup" location="회원가입" />
+      <CusBtn href="/CreateProj" location="만들기" />
+      <CusBtn href="/SearchProj" location="찾기" />
     </>
   );
 };
