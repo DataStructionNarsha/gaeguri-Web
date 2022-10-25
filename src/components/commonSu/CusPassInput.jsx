0e8dde51e036ce33a2f446e02620d48f9ae09777
sis01066745950 <67420558+sis01066745950@users.noreply.github.com>
2022-10-25 20:53:22 +0900

    비번 채크

--- src/components/commonSu/CusPassInput.jsx
+++ src/components/commonSu/CusPassInput.jsx
@@ -1,46 +1,39 @@
 import React,{useState} from 'react'
 
-
 const CusPassInput = ({type, placeholder1, placeholder2 }) => {
     const [pass, setPass] = useState("");
     const [passCh, setPassCh] = useState("");
     //const [change,setChange] = useState("");
+    const passChVal = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}/;
 
-    let tf = (true);
-    if(tf == true){
-        Tru();
-    }
-    else{
-        Fal();
+    
+    const passCh22 = () =>{
+      if(passChVal.test(pass)){
+        console.log("형식 O");
+      }else{
+        console.log("형식 X");
+      }
     }
 
     setTimeout(() => {
-        if(pass == passCh){
-            console.log("qqwrqgegnoevvenovonf");
-            //변수를 true로 바꾼ㅁ
-            tf = true;
-        }
-        else {
-            //변수를 else로 바꾼ㅁ
-            tf = false;
-        }
-    }, 0);
-
+        
+            if(pass == passCh && pass != ""){
+                console.log("success");
+            }else{
+                console.log('faild');
+                
+            }
+        
+        
+      }, "1")
   return (
     <>
-    <input type={type} placeholder={placeholder1} onChange={(e)=>setPass(e.target.value)} value={pass}/>
+    <input type={type} placeholder={placeholder1} onChange={(e)=>{setPass(e.target.value); passCh22()}} value={pass}/>
     <input type={type} placeholder={placeholder2} onChange={(e)=>{setPassCh(e.target.value);}} value={passCh}/>
 
     </>
   )
 }
-const  Tru = () => {
-    return(
-        <div>
-            password is same
-        </div>
-    )
-};
 
 
 
--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -3,8 +3,18 @@ import CusBtn from "./common/locationBtn";
 import "./common/signup.css";
 import CusChPass from "./commonSu/CusPassInput"
 
+const signup = () =>{
+  const emailJ = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+  /*const email = "";
+  const emailCh  = (e) =>{
+    if(emailJ.test(e)){
+      console.log("형식 O");
+    }else{
+      console.log("형식 X");
 
-
+    }
+  }*/
+  //usestate 사용해서 imput값 받아서 정규식으로 판별 해야함
   return (
     <>
       <div id="title" className="as">signup</div>
@@ -13,7 +23,7 @@ import CusChPass from "./commonSu/CusPassInput"
       <div className="Signup">
         <div className="Signup_center">
           <form action="#" method="post">
-            <input type="text" name = "email" placeholder="email"/>
+            <input type="text" name = "email" placeholder="email" id="email"/>
             <input type="text" name = "id" placeholder="id"/>
             <input type="text" name = "name" placeholder="name"/>
             <CusChPass type={"password"} placeholder1={"비밀번호를 입력해주세요"} placeholder2={"비밀번호 확인"} />
@@ -29,6 +39,6 @@ import CusChPass from "./commonSu/CusPassInput"
     </>
     
   );
-
+}
 
 export default signup;
