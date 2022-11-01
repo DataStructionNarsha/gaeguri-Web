4ad34f6d9cbf182c8e478267eb7586d93a5eb8f3
sis01066745950 <67420558+sis01066745950@users.noreply.github.com>
2022-11-01 20:21:26 +0900

    없어

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React,{useState} from "react";
 import CusBtn from "./common/locationBtn";
 
-const signIn = () => {
+const SignIn = () => { //대문자로 바꿈 회원가입도 마찬가지
+  const [passwordType, setPasswordType] = useState({ //password type 변경
+    type: 'password',
+    visible: false
+  });
+
+  //password type 변경하는 함수
+  const handlePasswordType = e => {
+      setPasswordType(() => {
+          if (!passwordType.visible) {
+              return { type: 'text', visible: true };
+          }
+          return { type: 'password', visible: false };
+      })
+    }
   return (
     <>
       <div>signIn</div>
       <CusBtn href="/Signup" location="회원가입" />
+
+      <form action="#" method="post">
+        <input type ="text" id = "email"/>
+        <input type ={passwordType.type} id = "password"/>
+        <span onClick={handlePasswordType}>
+          {  passwordType.visible ? <span>숨기기</span> : <span>보이기</span>  }
+        </span>
+        <input type="submit"/>
+      </form>
+
     </>
   );
 };
 
-export default signIn;
+export default SignIn;
