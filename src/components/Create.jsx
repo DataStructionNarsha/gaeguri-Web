8c5a66e688a86945f2ad14b06feda0559aa61ad8
bae0203 <gmbae06@dgsw.hs.kr>
2022-11-10 21:07:31 +0900

    ADD :: Date Picker
    달력 컴포넌트 추가

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,10 +1,27 @@
 import React, { useState, axios } from "react";
 import CusInput from "./common/CusInput";
+import Datepiker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { ko } from "date-fns/esm/locale";
+//import RadioBtn from "./common/radioBtn";
 let portCh = document.querySelector('input[name="purpose"]:checked');
 
 const Create = () => {
+  //달력 날짜
+  const [startDay, setStartDay] = useState(new Date());
+  const [endDay, setEndDay] = useState(new Date());
+
   const [page, setPage] = useState(0);
   const [ProjName, SetProjName] = useState("");
+
+  //라디오
+  const [radio, setRadio] = useState(0);
+  const handlerClRadioBtn = (e) => {
+    //라디오 버튼 핸들러
+    console.log(e.target.value);
+    setRadio(e.target.value);
+  };
+
   const page1 = () => {
     return (
       <>
@@ -19,14 +36,26 @@ const Create = () => {
         </nav>
         <nav id="purpose">
           <p>포트폴리오용 인가요 대회용인가요?</p>
-          <span>
-            <input type="radio" id="portfolio" name="purpose" />
-            <label htmlFor="portfolio">포트폴리오</label>
-          </span>
-          <span>
-            <input type="radio" id="competition" name="purpose" />
-            <label htmlFor="competition">대회</label>
-          </span>
+          <label>
+            <input
+              type="radio"
+              value={1}
+              name={"purpose"}
+              checked={radio === "1"}
+              onChange={handlerClRadioBtn}
+            />
+            {"포트폴리오"}
+          </label>
+          <label>
+            <input
+              type="radio"
+              value={2}
+              name={"purpose"}
+              checked={radio === "2"}
+              onChange={handlerClRadioBtn}
+            />
+            {"대회용"}
+          </label>
         </nav>
         <nav id="files">
           <p>프로젝트용 포스터가 있으면 넣어주세요</p>
@@ -42,11 +71,27 @@ const Create = () => {
         <p>당신이 선호하는 예상 일자와 마감 일자를 선택해주세요</p>
         <nav>
           <p>예상 일자</p>
-          <input type="date" />
+          <Datepiker
+            selected={startDay}
+            onChange={(date) => {
+              setStartDay(date);
+            }}
+            locale={ko}
+            dateFormat="yyyy년 MM월 dd일"
+          />
+          {/* <input type="date" /> */}
         </nav>
         <nav>
           <p>마감 일자</p>
-          <input type="date" />
+          <Datepiker
+            selected={endDay}
+            onChange={(date) => {
+              setEndDay(date);
+            }}
+            locale={ko}
+            dateFormat="yyyy년 MM월 dd일"
+          />
+          {/* <input type="date" /> */}
         </nav>
       </>
     );
@@ -57,12 +102,10 @@ const Create = () => {
       <>
         <p>마지막으로 당신이 원하는 팀원의 정보를 입력해주세요</p>
         <nav>
-          <header>
-            <span>포지션</span>
-            <span>스택</span>
-            <span>팀원 연령</span>
-            <span>명 수</span>
-          </header>
+          <span>포지션</span>
+          <span>스택</span>
+          <span>팀원 연령</span>
+          <span>명 수</span>
           <div>
             <select id="position">
               <option value="웹">웹</option>
@@ -98,6 +141,7 @@ const Create = () => {
     if (page < 2) {
       setPage((e) => (e = e + 1));
       console.log(page);
+      console.log(radio);
       // if (page == 0) {
       // } else if (page == 1) {
       // } else if (page == 2) {
