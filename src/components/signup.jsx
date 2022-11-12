9b016bd56f07c6cc0519849fea6d4983c08d3f85
sis01066745950 <67420558+sis01066745950@users.noreply.github.com>
2022-11-12 18:25:13 +0900

    form data ~~~

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,9 +1,11 @@
-import React, {useState} from "react";
+import React, {useState,} from "react";
+// import  post from "axios";
 import CusBtn from "./common/locationBtn";
 import "./common/signup.css";
+import axios from "axios";
+// import formData from "form-data";
 
 const Signup = () =>{
-  const emailJ = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
   const passChVal = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}/;
   const [pass, setPass] = useState(""); // 첫 번째 pssword
   const [passCh, setPassCh] = useState(""); // 두 번째 pssword
@@ -11,15 +13,36 @@ const Signup = () =>{
     type: 'password',
     visible: false
   });
-  let age = document.getElementById('age');
-  let id =document.getElementById('id');
-  let info =document.getElementById('info');
-  let password =document.getElementById('password');
-  let stac =document.getElementById('stac');
-  let profileimage =document.getElementById('profileimage');
+  var photoFile = document.getElementById("profileimage");
+  var state = { 
+    age : '',
+    id : '',
+    info : '',
+    password : '',
+    stac : '',
+    profileimage : '',
+  }
 
-
-//password type 변경하는 함수
+ //----------------------------------------------프로필 이미지 파일 선택 하면 미리보게 해주는 함수--------------------------------------------
+ const onChange = (e)=>{
+  var preview = document.querySelector('img');
+  let files = e.target.files;
+  console.log(files);
+  let reader = new FileReader();
+  reader.addEventListener(
+    'load',
+    function () {
+      preview.src = reader.result;
+    },
+    false
+  );
+  reader.onload = r => {
+       console.log(r.target.result);
+   };
+  reader.readAsDataURL(files[0]);
+ }
+//----------------------------------------------------------------------------------------------------------------------------------//
+//------------------------------------------------------password type 변경하는 함수----------------------------------------------------------------------//
 const handlePasswordType = e => {
     setPasswordType(() => {
         if (!passwordType.visible) {
@@ -28,8 +51,8 @@ const handlePasswordType = e => {
         return { type: 'password', visible: false };
     })
 }
-
-//password 정규식확인 함수
+/*--------------------------------------------------------------------------------------------------------------------------*/
+//-------------------------------------------------password 정규식확인 함수--------------------------------------------------//
   const passCh22 = () =>{
     if(passChVal.test(pass)){
       console.log("형식 O");
@@ -37,7 +60,8 @@ const handlePasswordType = e => {
       console.log("형식 X");
     }
   }
-  //password가 일치하는지 확인, email정규식 확인
+  /*--------------------------------------------------------------------------------------------------------------------------*/ 
+  //----------------------------------------------password가 일치하는지 확인, email정규식 확인-----------------------------------//
   setTimeout(() => {
         
     if(pass == passCh && pass != ""){
@@ -47,26 +71,30 @@ const handlePasswordType = e => {
     }
     
 
-
-
   }, "10")
-
+//---------------------------------------------------------------------------------------------------------------------------------------
+  //-----------------------------------------------------------서버에 보내기---------------------------------------------------------
   const handleJoin = () => {
-    fetch('http://10.80.163.109:5500/api/auth/signup', {
-      method: 'POST',
-      body: JSON.stringify({
-        "age": {age},
-        "id": {id},
-        "info": {info},
-        "password": {password},
-        "position": [
-          {stac}
-        ],
-        "profileimage": {profileimage} 
-      }),
-    })
+    
+    var url = 'http://10.80.163.109:5500/api/auth/signup';
+    var formData = new FormData();
+    formData.append('age',state.age);
+    formData.append('id',state.id);
+    formData.append('info',state.info);
+    formData.append('password',state.password);
+    formData.append('stac',state.stac);
+    formData.append("photo", photoFile.files[0]);
+    var config = {
+      headers : {
+        'context-type' : 'multipart/form-data'
+      }
+    }
+    axios.post(url,formData,config).then((req)=>{console.log(req)})
+    .then((req)=>{console.log(req)})
+    .catch((err)=>{console.log(err)});
+    
   }
-
+//-------------------------------------------------------------------------------------------------------------------------------------------------
   return (
     <>
       <div id="title" className="as">signup</div>
@@ -77,13 +105,13 @@ const handlePasswordType = e => {
 
           <form method="post">
   
-          <input type="number" name = "age" placeholder="age" id="age"/>
+            <input type="number" name = "age" placeholder="age" id="age" defaultValue={state.age}/>
 
-            <input type="text" name = "id" placeholder="id" id="id"/>
+            <input type="text" name = "id" placeholder="id" id="id" defaultValue={state.id}/>
 
-            <input type="text" name = "info" placeholder="자기소개" id="info"/>
+            <input type="text" name = "info" placeholder="자기소개" id="info" defaultValue={state.info}/>
 
-            <input type={passwordType.type} name = "password" placeholder="password" onChange={(e)=>{setPass(e.target.p1alue); passCh22()}} p1alue={pass} id="password"/>
+            <input type={passwordType.type} name = "password" placeholder="password" onChange={(e)=>{setPass(e.target.p1alue); passCh22()}} p1alue={pass} id="password" defaultValue={state.password}/>
 
             <span onClick={handlePasswordType}>
             	{  passwordType.visible ? <span>숨기기</span> : <span>보이기</span>  }
@@ -91,11 +119,12 @@ const handlePasswordType = e => {
 
             <input type="password" name = "password" placeholder="check password"  onChange={(e)=>{setPassCh(e.target.p1alue);}} p1alue={passCh}/>
 
-            <input type="text" name="stac" placeholder="stac" id="stac"/>
+            <input type="text" name="stac" placeholder="stac" id="stac" defaultValue={state.stac}/>
 
-            <input type="text" name="profile" placeholder="프사" id="profileimage"/>
-
-            <input type="button" onClick={handleJoin} name ="회원가입"/>
+            <input id="profileimage" type='file' name="profileimage" accept='image/jpg,impge/png,image/jpeg' onChange={onChange}>
+            </input>
+            <img src={onChange.file} alt="프로필 사진" height="200"></img>
+            <button type="button" onClick={handleJoin} name ="회원가입">올리기</button>
 
           </form>
 
