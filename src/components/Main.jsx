6d12168d221f8f85f1ded98f60927458b6413e83
bae0203 <gmbae06@dgsw.hs.kr>
2022-11-22 20:44:39 +0900

    ADD :: recoil추가
    recoil 추가
    NavBar 수정

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import CusBtn from "./common/locationBtn";
-import {Pobtn, Setbtn} from "./style/MainStyle"
+import { Pobtn, Setbtn } from "./Style/MainStyle";
 
 const Main = () => {
-
   return (
     <>
       <div>Main</div>
@@ -11,38 +10,38 @@ const Main = () => {
       <CusBtn href="/signup" location="회원가입" />
       <CusBtn href="/createproj" location="만들기" />
       <CusBtn href="/searchproj" location="찾기" />
-      
+
       <img src="" alt="" />
-        <p>포지션</p>
+      <p>포지션</p>
       <nav>
         <header>
-        <Pobtn>웹</Pobtn>
-        <Pobtn>서버</Pobtn>
-        <Pobtn>안드로이드</Pobtn>
-        <Pobtn>iOS</Pobtn>
-        <Pobtn>게임</Pobtn>
-        <nav className="Big_box">
-          <p>
+          <Pobtn>웹</Pobtn>
+          <Pobtn>서버</Pobtn>
+          <Pobtn>안드로이드</Pobtn>
+          <Pobtn>iOS</Pobtn>
+          <Pobtn>게임</Pobtn>
+          <nav className="Big_box">
             <p>
-              <Setbtn>웹</Setbtn>
-              <Setbtn>서버</Setbtn>
-              <Setbtn>안드로이드</Setbtn>
-              <Setbtn>IOS</Setbtn>
-              <Setbtn>게임</Setbtn>
-              <Setbtn>디자인</Setbtn>  
+              <p>
+                <Setbtn>웹</Setbtn>
+                <Setbtn>서버</Setbtn>
+                <Setbtn>안드로이드</Setbtn>
+                <Setbtn>IOS</Setbtn>
+                <Setbtn>게임</Setbtn>
+                <Setbtn>디자인</Setbtn>
+              </p>
             </p>
-          </p>
-          <nav>
-            <div className="dd">
-              <h2>Title</h2>
-              <p>Stack</p>
-              <p>내용</p>
-            </div>
-            <div>박스2</div>
-            <div>박스3</div>
-            <div>박스4</div>
+            <nav>
+              <div className="dd">
+                <h2>Title</h2>
+                <p>Stack</p>
+                <p>내용</p>
+              </div>
+              <div>박스2</div>
+              <div>박스3</div>
+              <div>박스4</div>
+            </nav>
           </nav>
-        </nav>
         </header>
       </nav>
     </>
